039b25229207514af53ce022f7a4e2ffcca05545
thomasholm1980 <thom.holm@gmail.com>
2026-03-04 19:50:11 +0100

    main

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,3 +2,5 @@
 export * from "./types";
 export { computeTrendReport } from "./trend/extractor";
 export { classifyState, classifySignal } from "./state/classifier";
+export { computeState } from "./state/pipeline";
+export { computeIntervention } from "./state/intervention";
