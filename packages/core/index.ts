0cbde4f654f78ed7e394adb3b2fd885bcddc258f
thomasholm1980 <thom.holm@gmail.com>
2026-03-05 13:59:21 +0100

    main

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -4,3 +4,5 @@ export { computeTrendReport } from "./trend/extractor";
 export { classifyState, classifySignal } from "./state/classifier";
 export { computeState } from "./state/pipeline";
 export { computeIntervention } from "./state/intervention";
+export { computePolicy } from "./policy/compute_policy";
+export type { PolicyDecisions, DailyStateInput, ReadinessBand, GuidanceIntensity } from "./policy/compute_policy";
