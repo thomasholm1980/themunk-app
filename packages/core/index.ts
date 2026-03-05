1d56cc797693c1d5c429a5e7f16e1ca4c934904e
thomasholm1980 <thom.holm@gmail.com>
2026-03-05 14:23:48 +0100

    main

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -6,3 +6,7 @@ export { computeState } from "./state/pipeline";
 export { computeIntervention } from "./state/intervention";
 export { computePolicy } from "./policy/compute_policy";
 export type { PolicyDecisions, DailyStateInput, ReadinessBand, GuidanceIntensity } from "./policy/compute_policy";
+export { assembleBrief } from "./renderer/assemble_brief";
+export type { DailyBrief } from "./renderer/assemble_brief";
+export { selectTemplate } from "./renderer/select_template";
+export { buildDisclaimer } from "./renderer/build_disclaimer";
