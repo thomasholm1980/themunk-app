26bdc32787820f9f79cc2a450953500021ddef24
Thomas <thomas@themunk.ai>
2026-03-07 15:31:02 +0100

    fix: export buildDailyBrief function from core index

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -36,4 +36,5 @@ export { buildLongitudinalSummary, computeStateDistribution, computeWindowAverag
 export type { LongitudinalStatus, TrajectoryDirection, ConfidenceLevel, DriverCode, DriftSummaryInput, DriftSummaryResult, WindowResult, WindowMetrics, StateDistribution, LongitudinalSummaryRecord, DailyStateRow, WearableLogRow, NervousSystemDriftRow, ProtocolAdherenceRow } from './longitudinal/types'
 export { DRIFT_ENGINE_VERSION, MIN_DAYS_7D, MIN_DAYS_14D } from './longitudinal/constants'
 export type { DailyBriefV1, DailyBriefConfidence, ObservationCode } from './domain/dailyBrief/types'
+export { buildDailyBrief } from './domain/dailyBrief/buildDailyBrief'
 export type { BuildDailyBriefV1Input } from './domain/dailyBrief/buildDailyBrief'
